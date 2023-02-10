60e1ab68ac94acb1d34b335e5306405e865afeef
jungle34 <joaovictorinter55@gmail.com>
2023-02-10 10:56:45 -0300

    Function for get Target Elements
    This function can return a object with the card, target, value and state from event handler element, this will be usefull when you need to disable or enable a input using a checkbox
    
    HTML Example:
    
    <div class="card-body>
       <input type="checkbox" data-target="child">
       <input type="text" id="child">
    </div>
    <div class="card-body>
       <input type="checkbox" data-target="child">
       <input type="text" id="child">
    </div>
    <div class="card-body>
       <input type="checkbox" data-target="child">
       <input type="text" id="child">
    </div>
    
    to call this function you need to set this parameters in call:
    _element = HTMLObjectElement of the checkbox
    _class = String of target div class name
    
    when you have the same id in multiples inputs is gonna be more easy with this function

---
+++ getTarget.js
@@ -0,0 +1,24 @@
+/**
+ * Function to get target elements in div
+ * 
+ * @param {HTMLObjectElement} _element 
+ * @param {String} _class 
+ * 
+ * @returns a list of target elements in div, they find the target if the element have data-target attribute
+ */
+function getCardTargets(_element, _class){
+	let card = $(_element).closest(`.${_class}`)[0];
+	let { target } = $(_element).data();
+	let state = $(_element).prop('checked');
+	let value = $(_element).val()
+	targetElement  = $(card).find(`#${target}`)[0];
+
+	return_data = {
+		card: card,
+		state: state,
+		value: value,
+		target: targetElement		
+	}
+
+	return return_data;
+}
